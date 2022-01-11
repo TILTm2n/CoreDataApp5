d908de351871bc1e1ff6914b899246d66702a9ed
Eugene <evgen29031999@bk.ru>
2022-01-11 17:40:32 +0300

    создание заготовки

--- CoreDataApp5/ViewController.swift
+++ CoreDataApp5/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let carNameArray = ["Lamborgini","Ferrari","Mersedes","Nissan","BMW",]
+    
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberofTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createSegmentedControl()
+    }
+    
+    func createSegmentedControl(){
+        segmentedControl.removeAllSegments()
+        segmentedControl.selectedSegmentTintColor = .green
+        var count = 0
+        for carName in carNameArray{
+            segmentedControl.insertSegment(withTitle: carName, at: count, animated: true)
+            
+            count += 1
+        }
+        
     }
 
-
+    @IBAction func segmentedCntrlPressed(_ sender: Any) {
+    }
+    
+    @IBAction func startEnginePressed(_ sender: Any) {
+    }
+    
+    @IBAction func rateItPressed(_ sender: Any) {
+    }
+    
 }
 
